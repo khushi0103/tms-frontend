192105a476867af387a67c10ce47fa21d2456a34
Sadikkhan02 <sadiksalmani2004@gmail.com>
2026-03-06 12:01:02 +0530

    now structured folder

--- src/features/platform/api/loginEndpoint.js
+++ src/features/platform/api/loginEndpoint.js
@@ -1,11 +1,20 @@
 import axiosInstance from "./axiosInstance"
 
 export const loginEndpoint = async (credentials)=>{
-    const response = await axiosInstance.post("admin/api/v1/auth/login/", credentials);
-    return response.data;
+    try {
+        const response = await axiosInstance.post("admin/api/v1/auth/login/", credentials);
+        return response.data;
+        
+    } catch (error) {
+        console.log(error);
+    }
 }
  
 export const refreshEndpoint = async (credentials)=>{
-    const response = await axiosInstance.post("admin/api/v1/auth/refresh/", credentials);
-    return response.data;
+    try {
+        const response = await axiosInstance.post("admin/api/v1/auth/refresh/", credentials);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
 }   
